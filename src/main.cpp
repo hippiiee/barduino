5d96d57f0de5ef8d111619bc4c3e61af1e9dea7e
hippiiee <hippolyte.q@gmail.com>
2021-03-10 17:24:48 +0100

    :wrench: add .idea to avoid problems when pulling the project with plateformio

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ void setup() {
     Serial.begin(57600);
 //    LEDS.addLeds<WS2812,DATA_PIN_LED,RGB>(leds,NUM_LEDS);
 //    LEDS.setBrightness(LED_BRIGHTNESS);
-    pinMode(stepPin, OUTPUT);
+   pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
 }
 
@@ -14,6 +14,7 @@ void loop() {
 //    ledLoading(8,24,30);
 //    ledFinished();
 //    ledStarting();
-    pushBottle();
+//    pushBottle();
+    goToBottle(1);
 }
 
--- src/motor.cpp
+++ src/motor.cpp
@@ -25,5 +25,26 @@ void pushBottle(){
 };
 
 void goToBottle(int bottleNumber){
+    //clockwise
+    digitalWrite(dirPin, HIGH);
+
+    for(int x = 0; x < 2000; x++){ // 200 is one turn
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(stepDelay);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(stepDelay);
+    }
+    delay(1000); // Wait a second
+    digitalWrite(dirPin, LOW);
+
+    // Spin motor
+    for(int x = 0; x < 2000; x++)
+    {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(stepDelay);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(stepDelay);
+    }
+    delay(1000); // Wait a second
 
 };
