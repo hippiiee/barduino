d1db6a915aeed67b20ac21deeebfc952264d745d
hippiiee <hippolyte.q@gmail.com>
2021-03-23 22:48:25 +0100

    :sparkles: bottle selection, moving plateform

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,31 @@
 #include "led.h"
 #include "motor.h"
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
-    Serial.begin(57600);
+    Serial.begin(9600);
 //    LEDS.addLeds<WS2812,DATA_PIN_LED,RGB>(leds,NUM_LEDS);
 //    LEDS.setBrightness(LED_BRIGHTNESS);
-   pinMode(stepPin, OUTPUT);
+    pinMode(stepPin, OUTPUT);
     pinMode(dirPin, OUTPUT);
+    pinMode(endSwitch, INPUT);
+    stepper.setMaxSpeed(1000);
+    goHome();
+    int drink[] = {3,4,6,1};
+    goToBottle(drink,4);
 }
-
+// bouteille 1 = -500
+// boutei@lle 2 = -1035
+// bouteille 2 = -1525
+// bouteille 2 = -2030
+// bouteille 2 = -2535
+// bouteille 2 = -3040
 void loop() {
-//    ledLoading(0,8,50);
+    //    ledLoading(0,8,50);
 //    ledLoading(8,24,30);
 //    ledFinished();
 //    ledStarting();
 //    pushBottle();
-    goToBottle(1);
+//    goToBottle(1);
 }
 
--- src/motor.cpp
+++ src/motor.cpp
@@ -24,27 +24,44 @@ void pushBottle(){
     delay(1000); // Wait a second
 };
 
-void goToBottle(int bottleNumber){
-    //clockwise
-    digitalWrite(dirPin, HIGH);
-
-    for(int x = 0; x < 2000; x++){ // 200 is one turn
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(stepDelay);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(stepDelay);
+void goHome(){
+    bool stopHome = false;
+    while(stepper.currentPosition() > -100){
+        stepper.setSpeed(-200);
+        stepper.runSpeed();
     }
-    delay(1000); // Wait a second
-    digitalWrite(dirPin, LOW);
-
-    // Spin motor
-    for(int x = 0; x < 2000; x++)
-    {
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(stepDelay);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(stepDelay);
+    stepper.setSpeed(400);
+    while(!stopHome){ //200 is one motor turn
+        if(digitalRead(endSwitch) == HIGH){
+            stopHome = true;
+        }else{
+            stepper.runSpeed();
+        }
     }
-    delay(1000); // Wait a second
+    stepper.setCurrentPosition(0);
+    while(stepper.currentPosition() != -15){
+        stepper.setSpeed(-50);
+        stepper.runSpeed();
+    }
+}
 
-};
+void goToBottle(int bottleNumber[],int size){
+    int position = 0;
+    stepper.setAcceleration(200);
+    stepper.setSpeed(-400);
+    Serial.println(sizeof(bottleNumber));
+    for(int i = 0; i < size; i++){
+        Serial.println(i);
+        position = posBottle[bottleNumber[i]-1];
+        if(stepper.currentPosition() != position){ //200 is one motor turn
+            if(digitalRead(endSwitch) == HIGH){
+                goHome();
+            }else{
+                stepper.moveTo(position);
+                stepper.runToPosition();
+                delay(1000);
+            }
+        }
+    }
+    goHome();
+}
