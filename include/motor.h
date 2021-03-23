f1b8139a421b14dd948d1ffb769c73b1ce61a9e6
hippiiee <hippolyte.q@gmail.com>
2021-03-23 22:47:00 +0100

    :heavy_plus_sign: new lib to control motors

--- include/motor.h
+++ include/motor.h
@@ -2,13 +2,20 @@
 #define BARDUINO_MOTOR_H
 
 #include <Arduino.h>
+#include "../lib/AccelStepper/src/AccelStepper.h"
 
-    static const int dirPin = 2;
-    static const int stepPin = 3;
-    static const int stepDelay = 500;
+#define dirPin 2
+#define stepPin 3
+#define motorInterfaceType 1
 
-    void pushBottle();
-    void goToBottle(int bottleNumber);
+static const int endSwitch = 4;
+static const int stepDelay = 1000;
+static const int posBottle[6] = {-500,-1035,-1525,-2030,-2535,-3040};
+extern AccelStepper stepper;
+
+void pushBottle();
+void goToBottle(int bottleNumber[], int size);
+void goHome();
 
 
 #endif //BARDUINO_MOTOR_H
