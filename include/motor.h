ef12b0c41eb900dbbd8b74f257843af1e1f5f529
Hippie <41185722+hippiiee@users.noreply.github.com>
2023-01-02 18:22:09 +0100

    💡 added comments

--- include/motor.h
+++ include/motor.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 #include "../lib/AccelStepper/src/AccelStepper.h"
-//#include "led.h"
 
 #define dirPin1 2
 #define stepPin1 3
@@ -11,8 +10,8 @@
 #define stepPin2 5
 #define motorInterfaceType 1
 
-static const int endSwitch = 8;
-static const int posBottle[6] = {-520,-1055,-1535,-2050,-2555,-3040};
+static const int endSwitch = 8; // the pin of your end switch
+static const int posBottle[6] = {-520,-1055,-1535,-2050,-2555,-3040}; // the position of your bottle based on the number of steps needed by your stepper motor 
 extern AccelStepper stepper1;
 extern AccelStepper stepper2;
 
