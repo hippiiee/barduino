7b3f5aa698fd11228a12878abf548d063086deee
Hippie <41185722+hippiiee@users.noreply.github.com>
2023-01-02 18:21:36 +0100

    💡 added comments

--- include/led.h
+++ include/led.h
@@ -4,9 +4,9 @@
 #include <Arduino.h>
 #include "../lib/FastLED/FastLED.h"
 
-static const int NUM_LEDS = 24;
-static const int DATA_PIN_LED = 7;
-static const int LED_BRIGHTNESS = 100;
+static const int NUM_LEDS = 24; // your number of leds on the ring
+static const int DATA_PIN_LED = 7; // your data pin
+static const int LED_BRIGHTNESS = 100; // the brightness of the leds
 extern CRGB leds[NUM_LEDS];
 
 void ledLoading(int min, int max, int time);
